8cd16896909bc1aa8687fdffd823ea53ba733592
Kodai Fujii <fujidai11@stu.kanazawa-u.ac.jp>
2021-04-22 16:06:12 +0900

    first commit

---
+++ plot.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 22 15:39:27 2021
+
+@author: kodai
+"""
+
---
+++ test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 14 03:18:41 2021
+
+@author: kodai
+"""
+
+print ("Hello")
+print('How are you ?')
+
+x="pokemon"
+print(x)
+
+x=True
+print(x)
+
+x=10
+if x>0 and x< 11 :
+    print(x)    
+
+print("Hello World")
